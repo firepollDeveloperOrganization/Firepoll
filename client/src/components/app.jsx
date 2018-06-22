95ef5e2e9702a4b87e20bc5f4db0092929a76425
Rose Lin <Rose.Lin@macaulay.cuny.edu>
2018-06-22 16:49:52 -0400

    wrap components in react-router

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import PollTestComponent from './pollTestComponent.jsx';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
     super(props); 
   }
-
   render() {
     return (
-      <div>
-        <div>Hello world!</div>
-        <PollTestComponent />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <div>
+            <div>Hello world!</div>
+            <PollTestComponent />
+          </div>
+          {/* <Route exact path="/login"> render={props => <Login {...props} /> } /> */}
+          {/* <Route exact path="/register"> render={props => <Register {...props} /> } /> */}
+          {/* <Route exact path="/polls/:id"> render={props => <Register {...props} />} /> */}
+          {/* <AuthRoute exact path="/auth" component={Auth} /> */}
+       </Switch>
+      </BrowserRouter>
     )
   }
-
 }
 
 export default App;
-
-//COMMENTING HERE FOR GIT PURPOSES
