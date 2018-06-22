5e197aaadbc6c97c548ea5ef377599a73a0ed0e4
rogersanick <rogers.a.nick@gmail.com>
2018-06-22 11:52:13 -0400

    Committing with git config

---
+++ client/src/pollTestComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PollTestComponent extends React.Component {
+  constructor(props) {
+    super(props); 
+  }
+
+  render() {
+    return (
+    <div>Hello I am to be used for testing socket functionality</div>
+    );
+  }
+
+}
+
+export default PollTestComponent;
+
+//COMMENTING HERE FOR GIT PURPOSES
