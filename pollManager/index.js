341a4a4cdf04b91e3050b94828bc04bd73259b24
rogersanick <rogers.a.nick@gmail.com>
2018-06-23 16:42:43 -0400

    Committing changes to dev branch

--- pollManager/index.js
+++ pollManager/index.js
@@ -3,7 +3,17 @@ const bodyParser = require('body-parser');
 const pollRouter = require('./routers/pollRouter');
 const resultHistRouter = require('./routers/resultsRouter');
 const cors = require('cors')
+require('dotenv').config();
 
+var config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
 
 const app = express();
 
