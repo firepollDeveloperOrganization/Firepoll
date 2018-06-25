deacc497913df3e372664a614852fcdaef6b937d
rogersanick <rogers.a.nick@gmail.com>
2018-06-25 14:42:04 -0400

    Adding functionality to post / retrieve testdata to firebase

--- pollManager/routers/stageRouter.js
+++ pollManager/routers/stageRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const stageRouter = express.Router();
 const firebase = require('../firebaseServerConfig/firebaseConfig.js');
+const testData = require('../pollTestData.js');
 require('dotenv').config();
 
 // Users should be able to create, and read results;
@@ -8,22 +9,19 @@ require('dotenv').config();
 db = firebase.database()
 
 stageRouter.post('/', (req, res) => {
-  db.ref('/question').set({
-    question: 'This is test question text',
-    answer: 'this is a test answer'
-  }).then(() => {
+  db.ref('/polls').set(testData).then(() => {
     res.status(200).send();
   }).catch((err) => {
     console.log(err);
   });
-})
+});
 
 stageRouter.get('/', (req, res) => {
-  console.log('someone wants a specific poll');
-  async () => {
-    var question = await db.ref('question/' + 'THISISATESTQUESTIONID').value();
+  var getPolls = async () => {
+    var question = await db.ref('/polls').once('value');
     res.status(200).send(question);
   }
-})
+  getPolls();
+});
 
 module.exports = stageRouter;
