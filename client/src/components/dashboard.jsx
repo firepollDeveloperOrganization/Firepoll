834a3f3de9ed05b86a3089d06c3ee72987c41082
Rose Lin <Rose.Lin@macaulay.cuny.edu>
2018-06-26 19:24:02 -0400

    build poll component

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
-import dummyData from '../../../pollManager/PollTestData.js';
+import Poll from './poll';
+import dummypolls from './dummydata';
+// import dummyData from '../../../pollManager/PollTestData.js';
+
+const sortByDateDescending = arr => {
+  return arr.sort((a, b) => {
+    if (a.timeMS < b.timeMS) return 1;
+    else return -1;
+  })
+}
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      polls: [dummyData, dummyData]
+      polls: sortByDateDescending(dummypolls)
     }
   }
   render() {
     let { user, email } = this.props;
     if (!user) return <Link to="/login"><button>Log In!</button></Link>;
       return (
-        <div>
-          <div>
+        <div id="dashboard">
+          <div className="nav">
             <h1>DASHBOARD: Welcome {user}!</h1>
             <h1>You are signing in with {email}</h1>
             <Link to="/create"><button>Create a poll!</button></Link>
             <button onClick={() => this.props.logout()}>Log Out</button>
           </div>
-          <div>
-
+          <div id="polls-container">
+            {this.state.polls.map(poll => <Poll key={poll.title} poll={poll} />)}
           </div>
         </div>
       )
---
+++ client/src/components/dummydata.js
@@ -0,0 +1,192 @@
+var dummypolls = [{
+  author: "username/userID",
+  title: "pollname1",
+  pollId: 3894723894,
+  staged: true,
+  complete: true,
+  executionDate: "Tue Jun 26 2018 18:56:19 GMT-0400 (Eastern Daylight Time)",
+  timeMS: '1530054086896',
+  totalAnswers: 1031,
+  winningAnswer: "answerId",
+  questions: [
+    {
+      questionId: "234234",
+      question: "What is your favorite dog?",
+      questionType: "multipleChoice",
+      answers: [
+        {
+          choiceId: "hexadecimalsomething",
+          choice: "Labrador",
+          responders: ["someClient", "anotherClient", "andSoOn"],
+          votes: 3
+        },
+        {
+          choiceId: "hexadecimalsomething",
+          choice: "Pitbull",
+          responders: [],
+          votes: 0
+        },
+        {
+          choiceId: "hexadecimalsomething",
+          choice: "Corgi",
+          responders: ["someClient", "anotherClient"],
+          votes: 2
+        },      
+      ]
+    },
+    {
+      questionId: "3243243",
+      question: "What is your favorite cat?",
+      questionType: "multipleChoice",
+      answers: [
+        {
+          answerId: "hexadecimalsomething",
+          choice: "Labrador",
+          responders: ["someClient", "anotherClient", "andSoOn"],
+          votes: 3
+        },
+        {
+          answerId: "hexadecimalsomething",
+          choice: "Pitbull",
+          responders: [],
+          votes: 0
+        },
+        {
+          answerId: "hexadecimalsomething",
+          choice: "Corgi",
+          responders: ["someClient", "anotherClient"],
+          votes: 2
+        }      
+      ]
+    }
+  ]
+},
+{
+  author: "username/userID",
+  title: "pollname2",
+  pollId: 2342343243,
+  staged: true,
+  complete: false,
+  executionDate: "6/25/2018, 3:28:30 PM",
+  timeMS: '1530054150024',
+  totalAnswers: 1031,
+  winningAnswer: "answerId",
+  questions: [
+    {
+      questionId: "234234",
+      question: "Do you like cookies?",
+      questionType: "multipleChoice",
+      answers: [
+        {
+          choiceId: "hexadecimalsomething",
+          choice: "Labrador",
+          responders: ["someClient", "anotherClient", "andSoOn"],
+          votes: 3
+        },
+        {
+          choiceId: "hexadecimalsomething",
+          choice: "Pitbull",
+          responders: [],
+          votes: 0
+        },
+        {
+          choiceId: "hexadecimalsomething",
+          choice: "Corgi",
+          responders: ["someClient", "anotherClient"],
+          votes: 2
+        },      
+      ]
+    },
+    {
+      questionId: "3243243",
+      question: "What about cupcakes?",
+      questionType: "multipleChoice",
+      answers: [
+        {
+          answerId: "hexadecimalsomething",
+          choice: "Labrador",
+          responders: ["someClient", "anotherClient", "andSoOn"],
+          votes: 3
+        },
+        {
+          answerId: "hexadecimalsomething",
+          choice: "Pitbull",
+          responders: [],
+          votes: 0
+        },
+        {
+          answerId: "hexadecimalsomething",
+          choice: "Corgi",
+          responders: ["someClient", "anotherClient"],
+          votes: 2
+        }      
+      ]
+    }
+  ]
+},
+{
+  author: "username/userID",
+  title: "pollname3",
+  pollId: 3242342323,
+  staged: false,
+  complete: false,
+  executionDate: "6/25/2018, 3:28:30 PM",
+  timeMS: '1530054172961',
+  totalAnswers: 1031,
+  winningAnswer: "answerId",
+  questions: [
+    {
+      questionId: "234234",
+      question: "What is love?",
+      questionType: "multipleChoice",
+      answers: [
+        {
+          choiceId: "hexadecimalsomething",
+          choice: "Labrador",
+          responders: ["someClient", "anotherClient", "andSoOn"],
+          votes: 3
+        },
+        {
+          choiceId: "hexadecimalsomething",
+          choice: "Pitbull",
+          responders: [],
+          votes: 0
+        },
+        {
+          choiceId: "hexadecimalsomething",
+          choice: "Corgi",
+          responders: ["someClient", "anotherClient"],
+          votes: 2
+        },      
+      ]
+    },
+    {
+      questionId: "3243243",
+      question: "Question?",
+      questionType: "multipleChoice",
+      answers: [
+        {
+          answerId: "hexadecimalsomething",
+          choice: "Labrador",
+          responders: ["someClient", "anotherClient", "andSoOn"],
+          votes: 3
+        },
+        {
+          answerId: "hexadecimalsomething",
+          choice: "Pitbull",
+          responders: [],
+          votes: 0
+        },
+        {
+          answerId: "hexadecimalsomething",
+          choice: "Corgi",
+          responders: ["someClient", "anotherClient"],
+          votes: 2
+        }      
+      ]
+    },
+
+  ]
+}];
+
+module.exports = dummypolls;
