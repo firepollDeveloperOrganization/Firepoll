4b952b010bed7eeeb09b2cbd8516073edc09241c
Rose Lin <Rose.Lin@macaulay.cuny.edu>
2018-06-27 10:49:28 -0400

    implement dynamic routing for live page

--- client/src/components/live.jsx
+++ client/src/components/live.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import dummypolls from './dummydata';
 
 class Live extends React.Component {
   constructor(props) {
-    super(props); 
+    super(props);
+    this.state = {
+      pollId: this.props.location.pathname.slice(6)
+    }
   }
+  
   render() {
+    console.log('props of live page', this.props);
+    console.log(this.state.pollId);
     let {user} = this.props;
-    // console.log('props of live', this.props);
-    if (!user) return <Redirect to='/login' />;
+    // if (!user) return <Redirect to='/login' />;
       return (
-        <div><h1>LIVE POLLS CONTROL PAGE COMPONENT</h1></div>
+        <div><h1>🔥🔥🔥 FIRE POLL #{this.state.pollId} 🔥🔥🔥</h1></div>
       )
   }
 }
