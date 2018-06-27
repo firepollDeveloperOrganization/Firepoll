7002579dac6ac8bb017f9464f2cc267244f04584
Rose Lin <Rose.Lin@macaulay.cuny.edu>
2018-06-27 11:18:58 -0400

    pass authenticated user info to live polls page for both client link and external

--- client/src/components/live.jsx
+++ client/src/components/live.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import dummypolls from './dummydata';
 
 class Live extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pollId: this.props.location.pathname.slice(6)
+      pollId: this.props.location.pathname.slice(6),
     }
   }
   
   render() {
-    console.log('props of live page', this.props);
-    console.log(this.state.pollId);
-    let {user} = this.props;
-    // if (!user) return <Redirect to='/login' />;
+    let {user, email} = this.props;
+    console.log('loading live poll', this.state.pollId, 'for', email);
+    if (!user) return <Link to="/login"><button>Log In!</button></Link>;
       return (
         <div><h1>🔥🔥🔥 FIRE POLL #{this.state.pollId} 🔥🔥🔥</h1></div>
       )
