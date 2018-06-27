91979b02f2b408aedba615a37f9837aa7b12bd40
laurentsmohr <laurents_mohr@yahoo.de>
2018-06-27 11:19:04 -0400

    feat(components/create.jsx): 955 functionality 0% styling
    need to add create poll button and function

--- client/src/components/create.jsx
+++ client/src/components/create.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
+import CreatedQuestions from './createdQuestions.jsx'
 
 class Create extends React.Component {
   constructor(props) {
@@ -23,20 +24,24 @@ class Create extends React.Component {
     //then reset current poll
     //then redirect to dashboard
   }
+
   addQuestion() {
+    console.log('addQuestion runs');
     var newQuestion = {
       question: this.state.currentQuestion,
       answers: this.state.answers
     }
     var allQuestions = this.state.questions;
     allQuestions.push(newQuestion);
+    console.log(allQuestions);
     this.setState({
-      questions: allQuestions,
       answers: [],
       currentQuestion: '',
-      currentAnswer: ''
+      currentAnswer: '',
+      questions: allQuestions
     })
   }
+  
   addAnswer(e) {
     e.preventDefault();
     let newAnswerArray = this.state.answers;
@@ -66,7 +71,7 @@ class Create extends React.Component {
       return (
         <div>
           {/*NAVBAR*/}
-          <div class="topnav">
+          <div className="topnav">
             <Link to="/dashboard"><button>Dashboard</button></Link>
             <button onClick={() => this.props.logout()}>Log Out</button>
           </div>
@@ -78,36 +83,34 @@ class Create extends React.Component {
           <div id="create-poll">
             <label className="label">Poll Title:</label>
             <div className="control">
-              <input className="input" type="text" id="pollName" value={this.state.pollName} onChange={this.handleChange} placeholder="Name your poll"/>
+              <input className="input" type="text" id="pollname" value={this.state.pollName} onChange={this.handleChange} placeholder="Name your poll"/>
             </div>
           </div>
-            {/*NEW QUESTION*/}
-            <div className="new-question box">
-              <div>Question #{this.state.questions.length + 1}</div>
-              <div className="field">
-                <div className="control">
-                  <input className="input" type="text" id="currentQuestion" value={this.state.currentQuestion} onChange={this.handleChange} placeholder="Type your question here" />
-                </div>
-              </div>
-              {/*CURRENT ANSWERS*/}
-              {this.state.answers.length > 0 &&
-                this.state.answers.map((answer, i) => {
-                  return (<li className="answer"><span>{answer}</span><button id={i.toString()} onClick={this.deleteAnswer}>delete</button></li>)
-                })
-              }
-              <form onSubmit={this.addAnswer} className="field">
-                <div className="control">
-                  <input className="input" type="text" id="currentAnswer"  value={this.state.currentAnswer} onChange={this.handleChange} placeholder="Type answer here to automatically add answer" />
-                </div>
-              </form>
-              <div className="addQuestionWrapper">
-                <button onClick={this.addQuestion}>Add Question</button>
+          {/*NEW QUESTION*/}
+          <div className="new-question box">
+            <div>Question #{this.state.questions.length + 1}</div>
+            <div className="field">
+              <div className="control">
+                <input className="input" type="text" id="currentQuestion" value={this.state.currentQuestion} onChange={this.handleChange} placeholder="Type your question here" />
               </div>
             </div>
-            {/*SIDE ELEMENT CREATED QUESTIONS*/}
-          <div id="created-questions-display">
-
+            {/*CURRENT ANSWERS*/}
+            {this.state.answers.length > 0 &&
+              this.state.answers.map((answer, i) => {
+                return (<li className="answer"><span>{answer}</span><button id={i.toString()} onClick={this.deleteAnswer}>delete</button></li>)
+              })
+            }
+            <form onSubmit={this.addAnswer} className="field">
+              <div className="control">
+                <input className="input" type="text" id="currentAnswer"  value={this.state.currentAnswer} onChange={this.handleChange} placeholder="Type answer here to automatically add answer" />
+              </div>
+            </form>
+            <div className="addQuestionWrapper">
+              <button onClick={this.addQuestion}>Add Question</button>
+            </div>
           </div>
+          {/*SIDE ELEMENT CREATED QUESTIONS*/}
+          <CreatedQuestions questions={this.state.questions}/>
         </div>
       )
   }
---
+++ client/src/components/createdQuestions.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class CreatedQuestions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedQuestion: 0
+    };
+    this.selectQuestion = this.selectQuestion.bind(this);
+  }
+  
+  selectQuestion(e) {
+    let i = parseInt(e.target.name);
+    this.setState({
+      selectedQuestion: i
+    })
+  }
+
+  render() {
+    if (this.props.questions.length === 0) {
+      return null;
+    } else {
+      return (
+        <div id="createdQuestionsWrapper">
+          <div id="createdQuestionsButtons">
+            {this.props.questions.map((question, index) => {
+              return (<button name={index} onClick={this.selectQuestion}>{`Question ${index}`}</button>)
+            })}
+          </div>
+          <div id="selectedQuestion">
+            <p id="selectedQuestionQuestion">
+              {this.props.questions[this.state.selectedQuestion].question}
+            </p>
+            <ul id="selectedQuestionAnswers">
+              {this.props.questions[this.state.selectedQuestion].answers.map((answer, index) => {
+                return (<li className="selectedQuestionAnswer" key={index}>{answer}</li>)
+              })}
+            </ul>
+          </div>  
+        </div>
+      )
+    }
+  }
+}
+
+export default CreatedQuestions;
