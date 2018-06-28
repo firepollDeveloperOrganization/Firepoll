5a24ab20102648c5a0ff4ea4afe3c61fc2ddb884
laurentsmohr <laurents_mohr@yahoo.de>
2018-06-28 16:38:22 -0400

    feat(components/dashboard.jsx): Dashboard now pulls actual user polls

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import Poll from './poll';
 import dummypolls from './dummydata';
+import axios from 'axios';
+import _ from 'underscore';
 // import dummyData from '../../../pollManager/PollTestData.js';
 
 const sortByDateDescending = arr => {
@@ -15,26 +17,78 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      allPolls: sortByDateDescending(dummypolls),
-      polls: sortByDateDescending(dummypolls)
+      allPolls: [],
+      filteredPolls: []
     }
-    this.deploy = this.deploy.bind(this);
-    this.filterPolls = this.filterPolls.bind(this);
+    
   }
-  deploy(pollId) {
-    console.log('deploying poll', pollId);
-    // send put request? to update poll to `staged` = true,
+
+  getPolls = () => {
+    axios.get(`/polls/user/${this.props.userId}`)
+    .then(res => {
+      console.log('users polls: ', res.data);
+      this.setState({allPolls: res.data, filteredPolls: res.data})
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+  
+  // When componentDidMount runs the first time, the props are not set yet, so that our db call would fail
+  // The interval is a VERY hacky solution to the problem
+  componentDidMount() {
+    console.log(this.props);
+    var that = this;
+    var interval = setInterval(() => {
+      console.log('running interval');
+      if(that.props.userId !== null) {
+        that.getPolls();
+        clearInterval(interval);
+      }
+    }, 200);
+  }
+
+  // ONE WAY TO SOLVE IT, KINDA RISKY
+  /*shouldComponentUpdate(nextProps, nextState) {
+    if(nextProps === this.props && this.state.allPolls.length > 0) {
+      if(nextState.filteredPolls !== nextState.allPolls) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate() {
+    this.getPolls();
+  }*/
+  
+  // THIS SHOULD RUN, BUT IT NEVER GETS RUN
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    console.log('will receive props', this.props);
+    //this.getPolls(this.props.userId);
+  }
+
+  
+  deploy = (index) => {
+    console.log('deploying poll w/ index', index);
+    // should send poll to firestore
+    // send db request to update poll to `staged` = true,
     // should trigger a rerender of the polls
   }
-  filterPolls(staged, completed) {
+
+  filterPolls = (staged, completed) => {
     console.log(`filtering for: staged ${staged}, completed ${completed}`);
     let filtered = this.state.allPolls.filter(poll => poll.completed === completed && poll.staged === staged);
-    this.setState({polls: filtered}, () => console.log('filtered polls!'));
+    this.setState({filteredPolls: filtered}, () => console.log('filtered polls!'));
   }
+
+
 
   render() {
     let { user, email } = this.props;
     if (!user) return <Link to="/login"><button>Log In!</button></Link>;
+    console.log('render function ', this.props);
       return (
         <div id="dashboard">
           <div className="nav">
@@ -50,7 +104,7 @@ class Dashboard extends React.Component {
             <button onClick={() => this.filterPolls(true, true)}>Show Only Completed</button>
           </div>
           <div id="polls-container">
-            {this.state.polls.map(poll => <Poll key={poll.title} poll={poll} deploy={this.deploy} />)}
+            {this.state.filteredPolls.map((poll, i) => <Poll key={i} index={i} poll={poll} deploy={this.deploy} />)}
           </div>
         </div>
       )
