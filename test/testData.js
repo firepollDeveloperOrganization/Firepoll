162b2cc31c132310c95042e9b765d4b597edbcc5
laurentsmohr <laurents_mohr@yahoo.de>
2018-07-09 11:03:53 -0400

    Fixing .gitignore mistake

---
+++ test/testData.js
@@ -0,0 +1,79 @@
+var poll = {
+  _id: "5b411bf3ffdf3b2250a94e21",
+  author: "VToWw3poEKMz1p5hD0nhNDnUdoC2",
+  title: "World Cup",
+  active: false,
+  completed: false,
+  num_questions: 1,
+  total_answers: 0,
+  winning_response: null,
+  start_time: null,
+  questions: [
+      {
+          question: "Who will win the World Cup?",
+          question_type: "multiple-choice",
+          total_voting_time: 10000,
+          _id: "5b411bf3ffdf3b2250a94e22",
+          answers: [
+              {
+                  choice: "Russia",
+                  votes: 0,
+                  _id: "5b411bf3ffdf3b2250a94e26",
+                  responders: []
+              },
+              {
+                  choice: "England",
+                  votes: 0,
+                  _id: "5b411bf3ffdf3b2250a94e25",
+                  responders: []
+              },
+              {
+                  choice: "France",
+                  votes: 0,
+                  _id: "5b411bf3ffdf3b2250a94e24",
+                  responders: []
+              },
+              {
+                  choice: "Belgium",
+                  votes: 0,
+                  _id: "5b411bf3ffdf3b2250a94e23",
+                  responders: []
+              }
+          ]
+      },
+      {
+        question: "Who will win the golden Boot?",
+        question_type: "multiple-choice",
+        total_voting_time: 10000,
+        _id: "5b411bf3ffdf3b2250a94e27",
+        answers: [
+            {
+                choice: "Lukaku",
+                votes: 0,
+                _id: "5b411bf3ffdf3b2250a94e31",
+                responders: []
+            },
+            {
+                choice: "Kane",
+                votes: 0,
+                _id: "5b411bf3ffdf3b2250a94e30",
+                responders: []
+            },
+            {
+                choice: "Mbape",
+                votes: 0,
+                _id: "5b411bf3ffdf3b2250a94e29",
+                responders: []
+            },
+            {
+                choice: "Cavani",
+                votes: 0,
+                _id: "5b411bf3ffdf3b2250a94e28",
+                responders: []
+            }
+        ]
+    }
+  ],
+}
+
+module.exports = poll;
