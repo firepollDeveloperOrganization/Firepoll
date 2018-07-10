a62092a2a933fc24869383c538af6d979a8a606f
Rose Lin <Rose.Lin@macaulay.cuny.edu>
2018-07-10 12:31:20 -0400

    work on more tests for dashboard

--- test/dashboard.spec.js
+++ test/dashboard.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount, configure } from 'enzyme';
+import { shallow, mount, render, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 import testData from './testData';
 import Dashboard from '../client/src/managementClient/dashboard.jsx';
@@ -20,15 +20,21 @@ describe('<Dashboard />', () => {
     const dashboard = shallow(<Dashboard user={'Rose'}/>);
     expect(dashboard.find('#dashboard').length).equal(1);
   });
-  it ('renders a login component if user is not logged in', () => {
+  it('renders a login component if user is not logged in', () => {
     const dashboard = shallow(<Dashboard user={null}/>);
     expect(dashboard.find('#dashboard').length).equal(0);
     expect(dashboard.find('Link').length).equal(1);
   });
-  it ('renders a heading that tells the user they have no polls if there are no polls', () => {
+  it.skip('renders a heading that tells the user they have no polls if there are no polls', () => {
     expect(0).equal(1);
   });
-  it ('renders a poll container if filtered polls exist', () => {
+  it.skip('renders a poll container if filtered polls exist', () => {
     expect(0).equal(1);
+  });
+  it ('renders four buttons ', () => {
+    const dashboard = render(<Dashboard user={'Rose'}/>);
+    // expect((dashboard.find('polls-filter button').length).equal(4));
+    expect(dashboard.find('polls-filter').children().length).equal(4);
+
   });
 })
