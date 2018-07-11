16234d5d4739be22b709c505714826810e0ab702
Rose Lin <Rose.Lin@macaulay.cuny.edu>
2018-07-11 19:37:04 -0400

    extract pie creator to separate visualizations file

--- client/src/managementClient/analytics.jsx
+++ client/src/managementClient/analytics.jsx
@@ -3,7 +3,7 @@ import { Redirect } from 'react-router-dom';
 import AnalyticsSub from './analyticsSub';
 import axios from 'axios';
 import Navbar from './navbar';
-// import Navbar from './navbar';
+import { createPie } from './visualizations';
 
 class Analytics extends React.Component {
   constructor(props) {
@@ -23,39 +23,9 @@ class Analytics extends React.Component {
       });
     });
     
-    // const createPie(divId, ) {
-
-    // }
-var pie = new d3pie("pieChart", {
-	"header": {
-		"title": {
-			"text": "Poll Votes",
-			"fontSize": 24,
-			"font": "open sans"
-		},
-		"subtitle": {
-			"color": "#999999",
-			"fontSize": 12,
-			"font": "open sans"
-		},
-		"location": "pie-center",
-		"titleSubtitlePadding": 9
-	},
-	"footer": {
-		"color": "#999999",
-		"fontSize": 10,
-		"font": "open sans",
-		"location": "bottom-left"
-	},
-	"size": {
-		"pieInnerRadius": "50%",
-		"pieOuterRadius": "70%"
-	},
-	"data": {
-		"sortOrder": "value-desc",
-		"content": [
+    createPie('pieChart', 'My Cool Poll', [
 			{
-				"label": "Vote5",
+				"label": "YES",
 				"value": 32170,
 				"color": "#f80000"
 			},
@@ -79,56 +49,8 @@ var pie = new d3pie("pieChart", {
 				"value": 78327,
 				"color": "#7a0b03"
 			}
-		]
-	},
-	"labels": {
-		"outer": {
-			"format": "label-value1",
-			"pieDistance": 32
-		},
-		"inner": {
-			"hideWhenLessThanPercentage": 3
-		},
-		"mainLabel": {
-			"fontSize": 11
-		},
-		"percentage": {
-			"color": "#ffffff",
-			"decimalPlaces": 0
-		},
-		"value": {
-			"color": "#adadad",
-			"fontSize": 11
-		},
-		"lines": {
-			"enabled": true
-		},
-		"truncation": {
-			"enabled": true
-		}
-	},
-	"tooltips": {
-		"enabled": true,
-		"type": "placeholder",
-		"string": "{label}: {percentage}%",
-		"styles": {
-			"fadeInSpeed": 348
-		}
-	},
-	"effects": {
-		"pullOutSegmentOnClick": {
-			"speed": 400,
-			"size": 8
-		}
-	},
-	"misc": {
-		"gradient": {
-			"enabled": true,
-			"percentage": 100
-		}
-	},
-	"callbacks": {}
-});
+		]);
+
 
   }
   render() {
--- client/src/managementClient/visualizations.js
+++ client/src/managementClient/visualizations.js
@@ -1,103 +1,79 @@
-var pie = new d3pie("pieChart", {
-	"header": {
-		"title": {
-			"text": "Poll Votes",
-			"fontSize": 24,
-			"font": "open sans"
-		},
-		"subtitle": {
-			"color": "#999999",
-			"fontSize": 12,
-			"font": "open sans"
-		},
-		"location": "pie-center",
-		"titleSubtitlePadding": 9
-	},
-	"footer": {
-		"color": "#999999",
-		"fontSize": 10,
-		"font": "open sans",
-		"location": "bottom-left"
-	},
-	"size": {
-		"pieInnerRadius": "50%",
-		"pieOuterRadius": "70%"
-	},
-	"data": {
-		"sortOrder": "value-desc",
-		"content": [
-			{
-				"label": "Vote5",
-				"value": 32170,
-				"color": "#f80000"
-			},
-			{
-				"label": "Vote4",
-				"value": 32170,
-				"color": "#fd938d"
-			},
-			{
-				"label": "Vote3",
-				"value": 36344,
-				"color": "#cb1b0e"
-			},
-			{
-				"label": "Vote2",
-				"value": 67706,
-				"color": "#a94545"
-			},
-			{
-				"label": "Vote1",
-				"value": 78327,
-				"color": "#7a0b03"
-			}
-		]
-	},
-	"labels": {
-		"outer": {
-			"format": "label-value1",
-			"pieDistance": 32
-		},
-		"inner": {
-			"hideWhenLessThanPercentage": 3
-		},
-		"mainLabel": {
-			"fontSize": 11
-		},
-		"percentage": {
-			"color": "#ffffff",
-			"decimalPlaces": 0
-		},
-		"value": {
-			"color": "#adadad",
-			"fontSize": 11
-		},
-		"lines": {
-			"enabled": true
-		},
-		"truncation": {
-			"enabled": true
-		}
-	},
-	"tooltips": {
-		"enabled": true,
-		"type": "placeholder",
-		"string": "{label}: {percentage}%",
-		"styles": {
-			"fadeInSpeed": 348
-		}
-	},
-	"effects": {
-		"pullOutSegmentOnClick": {
-			"speed": 400,
-			"size": 8
-		}
-	},
-	"misc": {
-		"gradient": {
-			"enabled": true,
-			"percentage": 100
-		}
-	},
-	"callbacks": {}
-});
+module.exports.createPie = (divId, title, dataSet) => {
+    var pie = new d3pie(divId, {
+      "header": {
+        "title": {
+          "text": title,
+          "fontSize": 24,
+          "font": "open sans"
+        },
+        "subtitle": {
+          "color": "#999999",
+          "fontSize": 12,
+          "font": "open sans"
+        },
+        "location": "pie-center",
+        "titleSubtitlePadding": 9
+      },
+      "footer": {
+        "color": "#999999",
+        "fontSize": 10,
+        "font": "open sans",
+        "location": "bottom-left"
+      },
+      "size": {
+        "pieInnerRadius": "50%",
+        "pieOuterRadius": "70%"
+      },
+      "data": {
+        "sortOrder": "value-desc",
+        "content": dataSet
+      },
+      "labels": {
+        "outer": {
+          "format": "label-value1",
+          "pieDistance": 32
+        },
+        "inner": {
+          "hideWhenLessThanPercentage": 3
+        },
+        "mainLabel": {
+          "fontSize": 11
+        },
+        "percentage": {
+          "color": "#ffffff",
+          "decimalPlaces": 0
+        },
+        "value": {
+          "color": "#adadad",
+          "fontSize": 11
+        },
+        "lines": {
+          "enabled": true
+        },
+        "truncation": {
+          "enabled": true
+        }
+      },
+      "tooltips": {
+        "enabled": true,
+        "type": "placeholder",
+        "string": "{label}: {percentage}%",
+        "styles": {
+          "fadeInSpeed": 348
+        }
+      },
+      "effects": {
+        "pullOutSegmentOnClick": {
+          "speed": 400,
+          "size": 8
+        }
+      },
+      "misc": {
+        "gradient": {
+          "enabled": true,
+          "percentage": 100
+        }
+      },
+      "callbacks": {}
+    });
+  }
