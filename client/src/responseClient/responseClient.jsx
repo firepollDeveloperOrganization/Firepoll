5bffd7e8edbbeb4f22ee1ce601e980297f39794c
rogersanick <rogers.a.nick@gmail.com>
2018-07-11 12:17:41 -0400

    Committing with updates

--- client/src/responseClient/responseClient.jsx
+++ client/src/responseClient/responseClient.jsx
@@ -195,7 +195,6 @@ class ResponseClient extends React.Component {
   // GETS RENDERED IF POLL IS LIVE
   renderLivePoll = () => {
     return (
-      
     <div id="poll-dist" className = "poll-dist-class">
       {this.state.pollComplete ? '' : this.state.poll ? <div className = "response-form">
         { this.state.questions ? this.state.questions.filter((ele, i) => i === this.state.currQuestion).map((question) => {
@@ -203,12 +202,15 @@ class ResponseClient extends React.Component {
               return (this.state.reveal ?
                 <div className = "question-container"> 
                   <MultipleChoiceQuestion currChoice = {this.state.currChoice} question = {question} handleUserChoice = {this.handleUserChoice} handleSubmit = {(e, question) => this.handleSubmit(e, question)}/>
-                  <button className="draw meet" onClick = {(e) => {this.handleSubmit(e, question)}}>Submit</button>
+                  <button className="draw meet submit" onClick = {(e) => {this.handleSubmit(e, question)}}>Submit</button>
                 </div>: 
                 <QuestionIntro question = {question} questionIntroLeave = {this.state.questionIntroLeave}/>);
             } else {
               return (
-                <div>Waiting for next question...</div>
+                <div className = "question-container">
+                  <div className = "loader"></div>
+                  <div className = "status">Waiting for next question...</div>
+                </div>
               );
             }
           })
@@ -250,6 +252,7 @@ class ResponseClient extends React.Component {
     } else {
       if (this.state.loading === true) {
         var status = "LOADING ...";
+        var loader = true;
       } else {
         let isScheduledText = "This poll is not yet live. Please wait for the host to start the poll and refresh this page.";
         let doesNotExistText = "We can't find the poll you are looking for. Try checking the link for typos.";
@@ -260,7 +263,8 @@ class ResponseClient extends React.Component {
       <div id = "poll-dist" className = "poll-dist-class">
         <div className = "response-form">
           <div className = "question-container">
-            <p>{status}</p>
+            <div className = "loader"></div>
+            <p className = "status">{status}</p>
           </div>
         </div>
       </div>
